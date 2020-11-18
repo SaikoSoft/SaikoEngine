e6d50692c7be9c3a8eb50d38b1a56fbc45c85850
Gollum999 <gollum9999@gmail.com>
2020-11-17 20:52:54 -0600

    Demo a C++20 feature

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@
 
 using json = nlohmann::json;
 
+struct Foo {
+    int i = 0;
+    int j = 0;
+
+    auto operator<=>(const Foo& rhs) const = default;
+};
+
 int main(int argc, char* argv[]) {
     std::cout << "Hello world\n";
     fmt::print("Hello fmt!\n");
@@ -20,6 +27,19 @@ int main(int argc, char* argv[]) {
     )"_json;
     std::cout << "JSON:\n" << j.dump(4) << "\n";
     std::cout << "lcm(5, 6) = " << boost::integer::lcm(5, 6) << "\n";
+
+    Foo f1{1, 2};
+    Foo f2{2, 1};
+    Foo f3{2, 2};
+
+    std::cout << "f1 < f2: " << (f1 < f2) << '\n';
+    std::cout << "f1 > f2: " << (f1 > f2) << '\n';
+    std::cout << "f2 < f3: " << (f2 < f3) << '\n';
+    std::cout << "f2 > f3: " << (f2 > f3) << '\n';
+    std::cout << "f1 == f2: " << (f1 == f2) << '\n';
+    std::cout << "f1 != f2: " << (f1 != f2) << '\n';
+    std::cout << "f2 == f3: " << (f2 == f3) << '\n';
+    std::cout << "f2 != f3: " << (f2 != f3) << '\n';
 
     return 0;
 }
