1db602e076abe344abadcb247019ef3836dd215f
Gollum999 <gollum9999@gmail.com>
2020-12-05 22:23:32 -0600

    Creating an empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,39 @@
-#include <fmt/core.h>
-#include <nlohmann/json.hpp>
-#include <spdlog/spdlog.h>
-#include <boost/integer/common_factor_rt.hpp>
+#include <Corrade/Utility/DebugStl.h>
+#include <Magnum/GL/Context.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/GL/Version.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/Platform/Sdl2Application.h>
 
-#include <iostream>
+using namespace Magnum;
 
-using json = nlohmann::json;
+class MyApplication: public Platform::Application
+{
+public:
+    explicit MyApplication(const Arguments &arguments);
 
-struct Foo {
-    int i = 0;
-    int j = 0;
-
-    auto operator<=>(const Foo& rhs) const = default;
+private:
+    void drawEvent() override;
 };
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello world\n";
-    fmt::print("Hello fmt!\n");
-    spdlog::info("Welcome to spdlog!");
+MyApplication::MyApplication(const Arguments& arguments)
+    : Platform::Application{arguments}
+{
+    using namespace Math::Literals;
+    GL::Renderer::setClearColor(0xdead69_rgbf);
 
-    auto j = R"(
-        {
-            "happy": true,
-            "pi": 3.141
-        }
-    )"_json;
-    std::cout << "JSON:\n" << j.dump(4) << "\n";
-    std::cout << "lcm(5, 6) = " << boost::integer::lcm(5, 6) << "\n";
-
-    Foo f1{1, 2};
-    Foo f2{2, 1};
-    Foo f3{2, 2};
-
-    std::cout << "f1 < f2: " << (f1 < f2) << '\n';
-    std::cout << "f1 > f2: " << (f1 > f2) << '\n';
-    std::cout << "f2 < f3: " << (f2 < f3) << '\n';
-    std::cout << "f2 > f3: " << (f2 > f3) << '\n';
-    std::cout << "f1 == f2: " << (f1 == f2) << '\n';
-    std::cout << "f1 != f2: " << (f1 != f2) << '\n';
-    std::cout << "f2 == f3: " << (f2 == f3) << '\n';
-    std::cout << "f2 != f3: " << (f2 != f3) << '\n';
-
-    return 0;
+    Debug{} << "Hello! This application is running on"
+            << GL::Context::current().version() << "using"
+            << GL::Context::current().rendererString();
 }
+
+void MyApplication::drawEvent() {
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+
+    // TODO: Add your drawing code here
+
+    swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(MyApplication)
